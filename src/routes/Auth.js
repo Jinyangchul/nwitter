91fe301edd3acd07367ce04663ca9f7d2a181281
Yeonho Cho <nanyeonho@gmail.com>
2023-01-20 13:06:19 +0900

    2.1 Login Form #1
    how to control forms
    
    -> set input's name, value
    
    -> create only one on change fn
    
    -> const { target: { name, value }} = event;

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { authService } from "../fbase";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      } else {
+        data = await signInWithEmailAndPassword(authService, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const tooggleAccount = () => setNewAccount((prev) => !prev);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          required
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          required
+          placeholder="Password"
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          required
+          value={newAccount ? "Create Account" : "Log In"}
+        />
+        {error}
+      </form>
+      <span onClick={tooggleAccount}>
+        {newAccount ? "Log in" : "Create Account"}
+      </span>
+      <div>
+        <button>Continue With Google</button>
+        <button>Continue With Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
